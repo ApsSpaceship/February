9ca3b9f3f6d4487c62f6ebdacfea266f25333ef8
naryeong-ko <99406992+naryeong-ko@users.noreply.github.com>
2022-02-15 23:12:53 +0900

    solve:P2007

---
+++ 0217/SWEA/P2007/P2007_ko.java
@@ -0,0 +1,30 @@
+package Study_20220211_17;
+
+import java.util.Scanner;
+
+public class SWEA_2007_패턴마디의길이 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			char[] str = sc.next().toCharArray(); //문자열 입력
+			int idx1 = 0; //idx1은 1번 마디의 인덱스
+      int idx2 = 1; //idx2는 2번 마디의 시작
+      
+			//1번 마디의 마지막 인덱스+1 == 2번 마디의 시작이면 찾은 것 && 마디의 최대 길이는 10
+			while (idx1 != idx2 && idx2 < 10) { 
+				idx1 = 0; //idx1 초기화
+				while (idx1 < idx2) {//값이 일치하면 2번 마디 시작 전까지 1번 마디 인덱스 증가하여 계속 비교
+					if (str[idx1] != str[idx2 + idx1]) { //값이 불일치하면 
+						idx2++; //2번 마디 위치 변경
+						break;
+					}
+					idx1++; 
+				}
+			}
+      
+			System.out.printf("#%d %d\n", tc, idx2); //2번 마디 시작위치 == 마디 길이
+		}
+  }
+}
