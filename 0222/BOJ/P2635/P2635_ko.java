e0fbe67744b7f11649bf83aee966a2047622a00a
naryeong-ko <99406992+naryeong-ko@users.noreply.github.com>
2022-02-19 18:59:41 +0900

    solve:P2635

---
+++ 0222/BOJ/P2635/P2635_ko.java
@@ -0,0 +1,40 @@
+package Study_20220218_24;
+
+import java.util.Scanner;
+
+public class BOJ_2635_수이어가기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count;
+		int idx;
+		int cMax = 0;
+		int[] numsMax = new int[30000]; //그냥 넉넉히 잡음
+		int[] nums = new int[30000];
+		
+		//계산
+		for (int i = N; i >= (N+1) / 2; i--) {// 처음 뺄 값은 절반보다 크게
+			count = 1; // N
+			idx = 0;
+			nums[idx++] = N; // 매번 배열 0은 N임
+			nums[idx] = i; // 매번 배열 1은 처음 뺄 값
+			while (nums[idx++] >= 0) { // 전 인덱스가 0보다 크다면
+				count++; // 개수 증가
+				nums[idx] = nums[idx - 2] - nums[idx - 1]; // 이번 인덱스 계산
+			}
+			if (cMax < count) {// count개수가 기존 최댓값보다 더 크면 이번 값들 numsMax에 저장
+				cMax = count;
+				for (int j = 0; j < count; j++) {
+					numsMax[j] = nums[j];
+				}
+			}
+		}
+		
+		//출력
+		System.out.println(cMax);
+		for (int i = 0; i < cMax; i++) {
+			System.out.printf("%d ", numsMax[i]);
+		}
+	}
+}
